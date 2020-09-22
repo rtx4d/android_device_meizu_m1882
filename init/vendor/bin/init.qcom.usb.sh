11e6822cc52c6f962247318130eddda005482b11
TRANSAERO21 <martynenkodima21@gmail.com>
2020-09-22 20:29:50 +0300

    m1882: init: Fix usb product string

--- init/vendor/bin/init.qcom.usb.sh
+++ init/vendor/bin/init.qcom.usb.sh
@@ -151,7 +151,7 @@ if [ -d /config/usb_gadget ]; then
 	msm_serial=`cat /sys/devices/soc0/serial_number`;
 	msm_serial_hex=`printf %08X $msm_serial`
 	machine_type=`cat /sys/devices/soc0/machine`
-	product_string="$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
+	product_string=`getprop ro.product.model`
 	echo "$product_string" > /config/usb_gadget/g1/strings/0x409/product
 
 	# ADB requires valid iSerialNumber; if ro.serialno is missing, use dummy
