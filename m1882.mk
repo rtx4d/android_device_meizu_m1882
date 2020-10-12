bd4c32e89464ee0e20f60b8390d8f71c98b44eb5
TRANSAERO21 <martynenkodima21@gmail.com>
2020-10-12 15:01:52 +0300

    m1882: sensors: Drop custom sensors HAL

--- m1882.mk
+++ m1882.mk
@@ -12,10 +12,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-product
 
-# Sensors
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-service.m1882
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
