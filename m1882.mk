eea4a52500779918a1ae3fe4fbd9270208ad3864
Andrey Sprynga <imolo4nik@gmail.com>
2020-10-30 22:27:58 +0700

    m1882: nuke lineage touch

--- m1882.mk
+++ m1882.mk
@@ -259,10 +259,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm845
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.pa.touch@1.0-service.meizu_m1882
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
