cfe2cd6a9dc61d2180be911efa5a075b8cfa299e
Andrey Sprynga <imolo4nik@gmail.com>
2020-10-30 22:16:59 +0700

    m1882: add qti components test

--- m1882.mk
+++ m1882.mk
@@ -210,6 +210,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    av \
+    bt \
+    perf \
+    wfd
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
