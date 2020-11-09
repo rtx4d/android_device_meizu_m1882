4c3454faa021551f803a87cea7fb708c1a10fbe6
android-usb <pmclean@google.com>
2020-11-09 18:45:36 +0700

    m1882: Adding Audio HAL V5: Direction API Bug: 119137468 Test: With testbed app

--- m1882.mk
+++ m1882.mk
@@ -61,7 +61,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
